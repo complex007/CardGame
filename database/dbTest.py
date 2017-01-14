35b86cd6c92d4a5f7d2946eb283eeacd42d386c4
complex007 <iris_yuqi.wang@outlook.com>
2017-01-14 11:07:52 +0800

    change

--- database/dbTest.py
+++ database/dbTest.py
@@ -3,11 +3,11 @@
 import dbImpl
 import playcardclasses
 dbtest=dbImpl.dbImpl()
-player1=playcardclasses.Player(3,"test3",2,"tese.jpg")
+# player1=playcardclasses.Player(3,"test3",2,"tese.jpg")
 # dbtest.createPlayer(player1)
 # dbtest.selectAll()
 # player2= dbtest.getPlayerById(3)
 # print player2.avatar
 # print dbtest.updatePlayer(player1).name
-dbtest.deletePlayerById(0)
+# dbtest.deletePlayerById(0)
 dbtest.selectAll()
